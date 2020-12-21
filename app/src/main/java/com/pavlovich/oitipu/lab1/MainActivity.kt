dd59cb951ef4117af3af2f31e204b56398611a1d
Pavlovich Nikolai <nipvvlun07@mail.ru>
2020-12-21 23:19:53 +0300

    input

---
+++ app/src/main/java/com/pavlovich/oitipu/lab1/MainActivity.kt
@@ -0,0 +1,170 @@
+package com.pavlovich.oitipu.lab1
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private var isOperation = false
+
+    private val addDigitToInputExpression = View.OnClickListener {
+        viewModel.input.value += (it as AppCompatButton).text
+        isOperation = false
+    }
+
+    private val addOperationToInputExpression = View.OnClickListener {
+        if (!isOperation)
+        {
+            viewModel.input.value += (it as AppCompatButton).text
+            isOperation = true
+        }
+    }
+
+
+    private val addSignToInputExpression = View.OnClickListener {
+        viewModel.input.value += " ${(it as AppCompatButton).text} "
+    }
+
+    private val addFunctionWithOpeningBracket = View.OnClickListener {
+        viewModel.input.value += "${(it as AppCompatButton).text}("
+    }
+
+    private val absClickListener = View.OnClickListener {
+        viewModel.input.value += "|"
+    }
+
+    private val reverseClickListener = View.OnClickListener {
+        viewModel.input.value = "1/(${viewModel.input.value})"
+    }
+
+    private val squareClickListener = View.OnClickListener {
+        viewModel.input.value += "^2"
+    }
+
+    private val powerClickListener = View.OnClickListener {
+        viewModel.input.value += "^"
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tv_input_text.apply {
+            movementMethod = ScrollingMovementMethod()
+        }.setHorizontallyScrolling(true)
+
+        if (isPortraitOrientation()) {
+            initPortraitOrientationListeners()
+        } else {
+            initLandscapeOrientationListeners()
+        }
+    }
+
+    private fun initLandscapeOrientationListeners() {
+        btn_left_bracket.setOnClickListener(addDigitToInputExpression)
+        btn_right_bracket.setOnClickListener(addDigitToInputExpression)
+        btn_seven.setOnClickListener(addDigitToInputExpression)
+        btn_eight.setOnClickListener(addDigitToInputExpression)
+        btn_nine.setOnClickListener(addDigitToInputExpression)
+        btn_four.setOnClickListener(addDigitToInputExpression)
+        btn_five.setOnClickListener(addDigitToInputExpression)
+        btn_six.setOnClickListener(addDigitToInputExpression)
+        btn_one.setOnClickListener(addDigitToInputExpression)
+        btn_two.setOnClickListener(addDigitToInputExpression)
+        btn_three.setOnClickListener(addDigitToInputExpression)
+        btn_zero.setOnClickListener(addDigitToInputExpression)
+        btn_point.setOnClickListener(addDigitToInputExpression)
+        btn_add.setOnClickListener(addOperationToInputExpression)
+        btn_subtract.setOnClickListener(addOperationToInputExpression)
+        btn_multiply.setOnClickListener(addOperationToInputExpression)
+        btn_divide.setOnClickListener(addOperationToInputExpression)
+
+        btn_sin?.setOnClickListener(addFunctionWithOpeningBracket)
+        btn_cos?.setOnClickListener(addFunctionWithOpeningBracket)
+        btn_tan?.setOnClickListener(addFunctionWithOpeningBracket)
+        btn_asin?.setOnClickListener(addFunctionWithOpeningBracket)
+        btn_acos?.setOnClickListener(addFunctionWithOpeningBracket)
+
+        btn_sqrt?.setOnClickListener(addFunctionWithOpeningBracket)
+        btn_exp?.setOnClickListener(addFunctionWithOpeningBracket)
+        btn_ln?.setOnClickListener(addFunctionWithOpeningBracket)
+
+        btn_abs?.setOnClickListener(absClickListener)
+
+        btn_reverse?.setOnClickListener(reverseClickListener)
+
+        btn_square?.setOnClickListener(squareClickListener)
+        btn_pow?.setOnClickListener(powerClickListener)
+
+        btn_clear.setOnClickListener {
+            viewModel.input.value = ""
+            viewModel.output.value = ""
+        }
+
+        btn_equals_hor?.setOnClickListener {
+            viewModel.calculateWithLib()
+        }
+
+        viewModel.input.observe(this, Observer<String> {
+            tv_input_text.text = it.replace(" ", "")
+        })
+
+        viewModel.output.observe(this, Observer<String> {
+            tv_output_text.text = it
+        })
+    }
+
+    private fun initPortraitOrientationListeners() {
+        btn_left_bracket.setOnClickListener(addDigitToInputExpression)
+        btn_right_bracket.setOnClickListener(addDigitToInputExpression)
+        btn_seven.setOnClickListener(addDigitToInputExpression)
+        btn_eight.setOnClickListener(addDigitToInputExpression)
+        btn_nine.setOnClickListener(addDigitToInputExpression)
+        btn_four.setOnClickListener(addDigitToInputExpression)
+        btn_five.setOnClickListener(addDigitToInputExpression)
+        btn_six.setOnClickListener(addDigitToInputExpression)
+        btn_one.setOnClickListener(addDigitToInputExpression)
+        btn_two.setOnClickListener(addDigitToInputExpression)
+        btn_three.setOnClickListener(addDigitToInputExpression)
+        btn_zero.setOnClickListener(addDigitToInputExpression)
+        btn_point.setOnClickListener(addDigitToInputExpression)
+
+        btn_multiply.setOnClickListener(addOperationToInputExpression)
+        btn_divide.setOnClickListener(addOperationToInputExpression)
+        btn_subtract.setOnClickListener(addOperationToInputExpression)
+        btn_add.setOnClickListener(addOperationToInputExpression)
+
+        btn_equals?.setOnClickListener {
+            viewModel.calculatePln()
+        }
+
+        btn_clear.setOnClickListener {
+            viewModel.input.value = ""
+            viewModel.output.value = ""
+        }
+
+        viewModel.input.observe(this, Observer<String> {
+            tv_input_text.text = it.replace(" ", "")
+        })
+
+        viewModel.output.observe(this, Observer<String> {
+            tv_output_text.text = it
+        })
+
+    }
+
+    private fun isPortraitOrientation() =
+        resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
+
+
+}
---
+++ app/src/main/java/com/pavlovich/oitipu/lab1/MainViewModel.kt
@@ -0,0 +1,180 @@
+package com.pavlovich.oitipu.lab1
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.*
+import javax.script.ScriptEngineManager
+import javax.script.ScriptException
+import kotlin.math.sqrt
+
+class MainViewModel : ViewModel() {
+
+    val input: MutableLiveData<String> by lazy {
+        MutableLiveData<String>().apply {
+            value = ""
+        }
+    }
+
+    val output: MutableLiveData<String> by lazy {
+        MutableLiveData<String>().apply {
+            value = ""
+        }
+    }
+
+    fun calculateWithLib() {
+        val inputValue = input.value ?: return
+
+        val scriptEngineManager = ScriptEngineManager()
+        val scriptEngine = scriptEngineManager.getEngineByName("rhino")
+
+        try {
+            val expression = inputValue
+                .replace(" ", "")
+                .replace("sin", "Math.sin")
+                .replace("cos", "Math.cos")
+                .replace("tan", "Math.tan")
+                .replace("√", "Math.sqrt")
+                .replace("exp", "Math.exp")
+                .replace("ln", "Math.log")
+                .replaceAbs()
+                //.replacePow()
+
+            val value = scriptEngine.eval(expression) as Double
+            println()
+            if (value.isNaN()) {
+                throw ScriptException("")
+            }
+
+            output.value =
+                if (value - value.toLong() == 0.0) value.toLong().toString() else value.toString()
+
+        } catch (e: ScriptException) {
+            output.value = "Expression cannot be evaluated :("
+        }
+    }
+
+    fun calculatePln() {
+        val values: Stack<Double> = Stack()
+        var lastSpace = 0
+        var secondValue: Double
+        var item: String
+
+        val pln = "${convertToReversePolish(input.value ?: "")} "
+
+        for (i in pln.indices) {
+
+            if (pln[i] == ' ') {
+                item = pln.substring(lastSpace, i)
+                item = item.replace("\\s+".toRegex(), "")
+
+                when (item) {
+                    "+" -> {
+                        values.push(values.pop() + values.pop())
+                    }
+                    "-" -> {
+                        secondValue = values.pop()
+                        values.push(values.pop() - secondValue)
+                    }
+                    "*" -> {
+                        values.push(values.pop() * values.pop())
+                    }
+                    "/" -> {
+                        secondValue = values.pop()
+                        values.push(values.pop() / secondValue)
+                    }
+                    "sqrt" -> {
+                        values.push(sqrt(values.pop()))
+                    }
+                    else -> {
+                        values.push(item.toDouble())
+                    }
+                }
+                lastSpace = i
+            }
+        }
+
+        output.value = if (values.peek() - values.peek().toLong() == 0.0)
+            values.peek().toLong().toString() else values.peek().toString()
+
+    }
+
+
+    private fun convertToReversePolish(exp: String?): String? {
+        if (exp == null) return null
+        var res = ""
+        val len = exp.length
+        val operator = Stack<Char>()
+        val reversePolish = Stack<String>()
+
+        operator.push('#')
+
+        var i = 0
+        while (i < len) {
+
+            while (i < len && exp[i] == ' ') i++
+
+            if (i == len) break
+
+            if (exp[i].isDigit()) {
+                var num = ""
+                while (i < len && exp[i].isDigit()) num += exp[i++]
+                reversePolish.push(num)
+            } else if (isOperator(exp[i])) {
+
+                when (val op = exp[i]) {
+                    '(' -> operator.push(op)
+                    ')' -> {
+                        while (operator.peek() != '(') reversePolish.push(
+                            operator.pop().toString()
+                        )
+                        operator.pop()
+                    }
+                    '+', '-' -> if (operator.peek() == '(') operator.push(op) else {
+                        while (operator.peek() != '#' && operator.peek() != '(') reversePolish.push(
+                            operator.pop().toString()
+                        )
+                        operator.push(op)
+                    }
+                    '*', '/' -> if (operator.peek() == '(') operator.push(op) else {
+                        while (operator.peek() != '#' && operator.peek() != '+' && operator.peek() != '-' && operator.peek() != '('
+                        ) reversePolish.push(operator.pop().toString())
+                        operator.push(op)
+                    }
+                }
+                i++
+            }
+        }
+        while (operator.peek() != '#') reversePolish.push(operator.pop().toString())
+        while (!reversePolish.isEmpty()) res =
+            if (res.isEmpty()) reversePolish.pop().toString() + res else reversePolish.pop()
+                .toString() + " " + res
+        return res
+    }
+
+    private fun String.replaceAbs() = buildString {
+        var inAbsBracket = false
+        for (c in this@replaceAbs) {
+            if (c == '|') {
+                inAbsBracket = !inAbsBracket
+                append(if (inAbsBracket) "Math.abs(" else ")")
+            } else
+                append(c)
+        }
+    }
+
+    private fun String.replacePow(): String {
+        var result = ""
+        val elements = this.split('+', '-', '*', '/', '=').toMutableList()
+        elements.forEach { s ->
+            if (s.contains('^')) {
+                val localSplit = s.split('^')
+                result = this.replace(s, "Math.pow(${localSplit[0]},${localSplit[1]})")
+            }
+        }
+
+        return result
+    }
+
+    private fun isOperator(c: Char) = c in "+-*/()"
+
+}
